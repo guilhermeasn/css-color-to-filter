2cded7529a6849543c2cdcb5e6597dca858a3173
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2024-10-20 13:14:19 -0300

    fix filter opacity

--- core/index.ts
+++ core/index.ts
@@ -13,8 +13,10 @@ export default function cssFilterData(color: ColorHex | ColorRgb): CSSFilterData
 }
 
 export function cssFilterOpacity(text: string, opacity : number) : string {
+    const end : boolean = /;\s*$/.test(text);
+    if(end) text = text.replace(/;\s*$/, '');
     text = text.replace(/\s?opacity\(.+?\)/, '');
-    return `${text} opacity(${opacity}%)`;
+    return `${text} opacity(${opacity}%)${end ? ';' : ''}`;
 }
 
 export {
