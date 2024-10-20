15f45443798a41212085c0cb5a0cf3364e579506
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2024-10-20 13:54:05 -0300

    fix color pick

--- components/FormColor.tsx
+++ components/FormColor.tsx
@@ -12,7 +12,13 @@ export default function FormColor({ color, setColor } : FormColorProps) {
 
     const mask = useCompleteMask(getPresetMask('COLOR_HEX'));
 
+    const [ timeoutNum, setTimeoutNum ] = useState<NodeJS.Timeout>();
     const [ maskColor, setMaskColor ] = useState<CompleteMask>(mask(color));
+
+    const onPickColor = (value : string) => {
+        clearTimeout(timeoutNum);
+        setTimeoutNum(setTimeout(() => setColor(value), 200));
+    }
 
     useEffect(() => Color.hexExpand(maskColor.result) !== color ? setMaskColor(mask(color)) : undefined, [ color ]);
     useEffect(() => maskColor.completed ? setColor(Color.hexExpand(maskColor.result)) : undefined, [ maskColor ]);
@@ -26,7 +32,7 @@ export default function FormColor({ color, setColor } : FormColorProps) {
                         <InputGroup.Text>#</InputGroup.Text>
                         <Form.Control className={ maskColor.completed ? undefined : 'is-invalid bg-danger-subtle text-danger' } type="text" value={ maskColor.result.replace('#', '') } onChange={ input => setMaskColor(mask(input.currentTarget.value)) } />
                     </InputGroup>
-                    <Form.Control className="ms-2" type="color" value={ color } onChange={ input => setColor(input.currentTarget.value) } />
+                    <Form.Control className="ms-2" type="color" value={ color } onChange={ input => onPickColor(input.currentTarget.value) } />
                 </div>
             </Container>
         </section>
